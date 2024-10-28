6cd9dacbba8da38b13e62c70e2ca181ffdf41ce5
Lê Bảo Duy <duypl572002@gmail.com>
2024-10-28 12:08:15 +0700

    add monster system

---
+++ Assets/Scripts/Data/ColorData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorData", menuName = "Color/ColorData")]
+public class ColorData : ScriptableObject
+{
+    public Color grey = Color.grey;
+    public Color green = Color.green;
+    public Color blue = Color.blue;
+    public Color red = Color.red;
+    public Color yellow = Color.yellow;
+
+    //public Color red = Color.red;
+    //public Color orange = new Color(255 / 255.0f, 127 / 255.0f, 0 / 255.0f);
+    //public Color yellow = Color.yellow;
+    //public Color green = Color.green;
+    //public Color blue = Color.blue;
+    //public Color indigo = new Color(75 / 255.0f, 0 / 255.0f, 130 / 255.0f);
+    //public Color violet = new Color(148 / 255.0f, 0 / 255.0f, 211 / 255.0f);
+
+}
---
+++ Assets/Scripts/Model/Monster.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Monster
+{
+    public int health = 1;
+    public float velocity = 3f;
+}
