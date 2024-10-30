e6b480dcba9299a0b90d63022a9811186c83fb50
TungNguyen <tungnguyenviet23062003@gmail.com>
2024-10-31 01:25:58 +0700

    Add ObjectiveUI

---
+++ Assets/Scripts/UI/ObjectiveUI.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveUI : MonoBehaviour
+{
+    [SerializeField] private LevelDataSO levelData;
+}
