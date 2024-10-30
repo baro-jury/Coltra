bed595a99b433fbb605f8274e899c54b56f233ae
MangoGamesVN <154311688+MangoGamesVN@users.noreply.github.com>
2024-10-30 17:25:58 +0700

    add font, update UI home

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    void MakeSingleInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    void Awake()
+    {
+        MakeSingleInstance();
+    }
+
+    public AudioSource soundSource;
+    public AudioSource musicSource;
+
+}
