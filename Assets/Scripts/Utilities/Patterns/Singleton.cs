700f33ca0638f7d3e3743cc274e9e12299f5ec41
MangoGamesVN <154311688+MangoGamesVN@users.noreply.github.com>
2024-11-01 13:45:26 +0700

    update boss, ingame controller

--- Assets/Scripts/Utilities/Patterns/Singleton.cs
+++ Assets/Scripts/Utilities/Patterns/Singleton.cs
@@ -7,7 +7,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     private static T _instance = null;
     public static T Instance => _instance;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         if (_instance == null)
         {
